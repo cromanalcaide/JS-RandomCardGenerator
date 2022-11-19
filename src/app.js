0414c5a4190ca0777621a28213c938fe85f92659
cromanalcaide <cromanalcaide@gmail.com>
2022-11-19 11:00:19 +0000

    randomizer + card generated + frustration over colors not changing

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["♦", "♥", "♠", "♣"];
+  //let suits = [1, 2, 3, 4];
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "J", "Q", "K"];
+  let randomSuit = Math.floor(Math.random() * suits.length);
+  let randomNumber = Math.floor(Math.random() * numbers.length);
+
+  // let suitInCard = document
+  //   .querySelector("suitAppeared")
+  //   .appendChild(suits[randomSuit]);
+  // let numberInCard = document
+  //   .querySelector("numberAppeared")
+  //   .appendChild(numbers[randomNumber]);
+
+  let suitInCard = document.createElement("p");
+  suitInCard.innerText = suits[randomSuit];
+  document.querySelector(".suitAppeared").appendChild(suitInCard);
+  // let suitInCard2 = document.createElement("p");
+  // suitInCard2.innerText = suits[randomSuit];
+  // document.querySelector(".suitAppeared2").appendChild(suitInCard);
+  let numberInCard = document.createElement("p");
+  numberInCard.innerText = numbers[randomNumber];
+  document.querySelector(".numberAppeared").appendChild(numberInCard);
+
+  if (suitInCard === "♠" && "♣") {
+    document.querySelector(".suitAppeared").style.color = "black";
+  } else {
+    document.querySelector(".suitAppeared").style.color = "red";
+  }
+
+  // console.log(suits[randomSuit]);
+  // console.log(numbers[randomNumber]);
 };
